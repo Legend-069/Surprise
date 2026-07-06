9cd0ca5c9b315a9855bd6fe35fae309fdfe18f1d
Legend-069 <deardiwakar.in@gmail.com>
2026-07-06 09:52:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!','Negar ' ],
+			strings: [ 'HAPPY', 'BIRTHDAY!','Ishita ' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
